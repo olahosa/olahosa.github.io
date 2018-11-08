1d693868c18aa8335008637a9c6cf679540960b0
a.hosa@columbia.edu <olahosa>
2018-11-08 16:00:12 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -12,7 +12,7 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.',
   'example4':
       'Alice was beginning to get very.',
   'example5':
