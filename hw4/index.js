4b18b27b2ed73b682a481579c9fca284989aaefa
a.hosa@columbia.edu <olahosa>
2018-11-27 23:10:54 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -8,17 +8,17 @@ const HOSTED_URLS = {
 
 const examples = {
   'example1':
-      'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife',
-  'example2':
-      'Darcy, as well as Elizabeth, really loved them; and they were both ever sensible of the warmest gratitude towards the persons who, by bringing her into Derbyshire, had been the means of uniting them',
-  'example3':
       'Happy families are all alike; every unhappy family is unhappy in its own way',
-  'example4':
+  'example2':
       'but my life now, my whole life apart from anything that can happen to me, every minute of it is no more meaningless, as it was before, but it has the positive meaning of goodness, which I have the power to put into it',
-  'example5':
+  'example3':
       'Thirty years ago, Marseilles lay burning in the sun, one day',
+  'example4':
+      'and as they passed along in sunshine and shade, the noisy and the eager, and the arrogant and the froward and the vain, fretted and chafed, and made their usual uproar',
+  'example5':
+      'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife',
   'example6':
-      'and as they passed along in sunshine and shade, the noisy and the eager, and the arrogant and the froward and the vain, fretted and chafed, and made their usual uproar'    
+      'Darcy, as well as Elizabeth, really loved them; and they were both ever sensible of the warmest gratitude towards the persons who, by bringing her into Derbyshire, had been the means of uniting them'
 };
 
 function status(statusText) {
